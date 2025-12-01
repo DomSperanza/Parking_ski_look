c2ebf9821b0ab382fbc21b617fdd7ade7ed46e92
Dom <my.email@example.com>
2025-11-30 17:45:41 -0700

    feat: Add '--disable-setuid-sandbox' option to Chrome options for enhanced security in headless mode.

--- monitoring/parking_scraper_v3.py
+++ monitoring/parking_scraper_v3.py
@@ -134,6 +134,7 @@ def get_driver(headless=True):
         if headless:
             chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-setuid-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
